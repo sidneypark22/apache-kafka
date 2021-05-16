34378c44ba6916977d59b6672e4ed1f97af1deec
Sidney Park <spa0220@gmail.com>
2021-05-16 21:56:20 +1200

    wip

--- kafka-twitter-streaming/twitter_producer.py
+++ kafka-twitter-streaming/twitter_producer.py
@@ -9,16 +9,15 @@ kafka_server = 'localhost:9092'
 
 BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAJbpIwEAAAAAk4Ed%2Bi6Bb11C5%2FhFXADeC24cPNg%3DZYcCYf3WfukS1YSnOS8VEzHBpqj2zVDmYhoZLO3vnhgnaAM0Uo"
 
-current_utc_datetime = datetime.utcnow()
-current_utc_datetime_rfc_3339 = current_utc_datetime.strftime("%Y-%m-%dT%H:%M:%SZ")
-from_datetime = '2021-05-14T00:00:00Z'
-to_datetime = current_utc_datetime_rfc_3339
-last_newest_id = "1393573728856989698"
+#current_utc_datetime = datetime.utcnow()
+#current_utc_datetime_rfc_3339 = current_utc_datetime.strftime("%Y-%m-%dT%H:%M:%SZ")
+#from_datetime = '2021-05-14T00:00:00Z'
+#to_datetime = current_utc_datetime_rfc_3339
 
-def search_twitter(query, tweet_fields, bearer_token = BEARER_TOKEN):
-    start_time = "start_time={}".format(from_datetime)
-    end_time = "end_time={}".format(to_datetime)
-    max_results = "max_results=10"
+def search_twitter(query, last_newest_id, number_of_results, tweet_fields, bearer_token = BEARER_TOKEN):
+    #start_time = "start_time={}".format(from_datetime)
+    #end_time = "end_time={}".format(to_datetime)
+    max_results = "max_results={}".format(number_of_results)
     since_id = "since_id={}".format(last_newest_id)
     
     headers = {"Authorization": "Bearer {}".format(bearer_token)}
@@ -37,14 +36,50 @@ def search_twitter(query, tweet_fields, bearer_token = BEARER_TOKEN):
         raise Exception(response.status_code, response.text)
     return response.json()
 
+
 query = "doge"
 #tweet_fields = "tweet.fields=text,author_id,created_at"
 tweet_fields = "tweet.fields=text,author_id,created_at,public_metrics&expansions=author_id&user.fields=description"
+last_newest_id = "1393573728856989698"
+number_of_results = 10
 
-json_response = search_twitter(query=query, tweet_fields=tweet_fields, bearer_token=BEARER_TOKEN)
+json_response = search_twitter(
+    query=query, 
+    last_newest_id=last_newest_id, 
+    number_of_results=number_of_results, 
+    tweet_fields=tweet_fields, 
+    bearer_token=BEARER_TOKEN
+)
 
-#json_response["meta"]["oldest_id"]
-#json_response["meta"]["newest_id"]
+def flatten_tweet_json_response(json_response):
+    tweet_list = []
+    for tweet in json_response["data"]:
+        tweet_flatten = {
+            'id': tweet['id']
+            , 'created_at': tweet['created_at']
+            , 'author_id': tweet['author_id']
+            , 'text': tweet['text']
+            , 'retweet_count': tweet['public_metrics']['retweet_count']
+            , 'reply_count': tweet['public_metrics']['reply_count']
+            , 'like_count': tweet['public_metrics']['like_count']
+            , 'quote_count': tweet['public_metrics']['quote_count']
+        }
+        tweet_list.append(tweet_flatten)
+    return tweet_list
+
+def users_json_response(json_response):
+    user_list = []
+    user_list = json_response["includes"]["users"]
+    return user_list
+
+
+tweet_list = flatten_tweet_json_response(json_response)
+user_list = users_json_response(json_response)
+
+oldest_id = json_response["meta"]["oldest_id"]
+newest_id = json_response["meta"]["newest_id"]
+
+
 
 print(json.dumps(json_response, indent=4, sort_keys=True))
 
