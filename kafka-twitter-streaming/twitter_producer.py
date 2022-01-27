f460519079bd3f9c253cb73a118a34a9a0b12baf
sidneypark22 <42913018+sidneypark22@users.noreply.github.com>
2022-01-28 00:34:21 +1300

    Update twitter_producer.py

--- kafka-twitter-streaming/twitter_producer.py
+++ kafka-twitter-streaming/twitter_producer.py
@@ -15,7 +15,7 @@ session = boto3.Session(
 s3_client = session.client('s3')
 s3_resource = session.resource('s3')
 
-bucket = 'spark22-kafka-streaming'
+bucket = ''
 
 #s3_client.list_objects(
 #    Bucket = bucket
@@ -23,10 +23,6 @@ bucket = 'spark22-kafka-streaming'
 
 s3_bucket = s3_resource.Bucket(bucket)
 s3_bucket_folder_name = "twitter-streaming/"
-
-#s3_objects = s3_client.list_objects(
-#    Bucket='spark22-20201228'
-#)
 
 #topic_name = 'tweets'
 #kafka_server = 'localhost:9092'
